f2b8e7883553f0d514fed9827e5c43103136ef44
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-11-22 19:46:09 -0700

    Password restrictions added

--- Planify/Models/User.cs
+++ Planify/Models/User.cs
@@ -15,11 +15,15 @@ namespace Planify.Models
         public ICollection<Role> Roles { get; set; } = new List<Role>();
     }
 
-    public class UserRegisterDTO
+
+    public class UserDTO
     {
         [EmailAddress]
         public required string Email { get; set; }
+        [MinLength(8, ErrorMessage = "The password must be 8 characters or more.")]
+        [MaxLength(32, ErrorMessage = "Password must be 32 characters maximum.")]
         public required string Password { get; set; }
+
     }
 
 }
