1008dc8e6002b44279adf2ddfcb655fbf5a50d4a
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-11-22 19:31:15 -0700

    DbContext added

---
+++ Planify/Data/ProjectContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Planify.Models;
+
+namespace Planify.Data
+{
+    public partial class ProjectContext : DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectTask> ProjectTasks { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+
+    public partial class ProjectContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            SetEmployeeTable(modelBuilder);
+        }
+
+        private void SetEmployeeTable(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Employee)
+                .WithOne(e => e.User)
+                .HasForeignKey<Employee>(e => e.UserId);
+        }
+    }
+}
