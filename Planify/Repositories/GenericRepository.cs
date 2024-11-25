a83a78d5cdfb18f396527628ffd05c098477a602
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-11-24 23:13:54 -0700

    Removing innecesary usings in Generic Repository implementation

--- Planify/Repositories/GenericRepository.cs
+++ Planify/Repositories/GenericRepository.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Planify.Data;
 using Planify.Models;
-using Planify.Services;
 using System;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 
