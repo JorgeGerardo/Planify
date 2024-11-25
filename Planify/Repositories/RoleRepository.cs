d6909f604ffb57132a73a2a4b1f50c2308d827ed
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-11-25 01:32:27 -0700

    Role controller added

---
+++ Planify/Repositories/RoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Planify.Data;
+using Planify.Models;
+
+namespace Planify.Repositories
+{
+    public class RoleRepository : GenericRepository<Role, int>
+    {
+        private ProjectContext _projectContext;
+        public RoleRepository(ProjectContext context) : base(context)
+        {
+            _projectContext = context;
+        }
+    }
+}
