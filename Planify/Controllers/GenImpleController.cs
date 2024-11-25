0b7cb92f90df2bce9b25ff2d0be7132aa7fae6f3
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-11-25 01:26:10 -0700

    Generic controller added

---
+++ Planify/Controllers/GenImpleController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Planify.Models;
+using Planify.Repositories;
+
+namespace Planify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public partial class DepartmentsController2 : GenericController<Department, DepartmentRepository>
+    {
+        public DepartmentsController2(IGenericCRUDRepository<Department, int> department) : base(department) { }
+
+    }
+
+    //POST & PUT
+    public partial class DepartmentsController2
+    {
+        //[HttpPost]
+        //public async Task<IActionResult> Add(DepartmentDTO newDeparment)
+        //{
+        //    Department? NewDepartment = await _department
+        //        .Create(new Department { Name = newDeparment.Name });
+
+        //    await _department.Save();
+
+        //    if (NewDepartment is null)
+        //        return StatusCode(500);
+
+        //    return CreatedAtAction(nameof(Get), new { NewDepartment.Id }, NewDepartment);
+        //}
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Update(int id, DepartmentDTO deparmentChanges)
+        //{
+        //    var dep = await _department.GetById(id);
+        //    if (dep is null)
+        //        return NotFound();
+
+        //    dep.Name = deparmentChanges.Name;
+        //    _department.Updated(dep);
+
+        //    ConcurrencyState state = await Concurrency.Check(() => _department.Save());
+
+        //    if (state == ConcurrencyState.ConcurrencyDetected)
+        //        return Conflict(new { message = ConflictMessage() });
+
+        //    return NoContent();
+        //}
+
+    }
+
+}
---
+++ Planify/Controllers/GenericController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Planify.Models;
+using Planify.Repositories;
+using Planify.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Planify.Controllers
+{
+    public partial class GenericController<T, TRepository> : ControllerBase
+        where T : BaseModel<int>
+        where TRepository : IGenericCRUDRepository<T, int>
+    {
+        private readonly IGenericCRUDRepository<T, int> _Repository;
+        public GenericController(IGenericCRUDRepository<T, int> context) =>
+            _Repository = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<T>> Get() =>
+            await _Repository.GetAll().ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<T>> GetById(int id)
+        {
+            T? res = await _Repository.GetById(id);
+            return res is not null ? res : NotFound();
+        }
+
+    }
+
+    public partial class GenericController<T, TRepository>
+    {
+        [HttpPut("{id}")]
+        public virtual async Task<IActionResult> UpdateName(int id, T deparmentChanges)
+        {
+            var dep = await _Repository.GetById(id);
+            if (dep is null)
+                return NotFound();
+
+            dep.Name = deparmentChanges.Name;
+            _Repository.Updated(dep);
+
+            ConcurrencyState state = await Concurrency.Check(() => _Repository.Save());
+
+            if (state == ConcurrencyState.ConcurrencyDetected)
+                return Conflict(new { message = Concurrency.ConflictMessage() });
+
+            return NoContent();
+        }
+
+
+    }
+
+    //DELETE:
+    public partial class GenericController<T, TRepository>
+    {
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            bool res = await _Repository.SoftDelete(id);
+            await _Repository.Save();
+
+            return res ? NoContent() : NotFound();
+        }
+
+        [HttpDelete("hardDelete/{id}")]
+        public async Task<IActionResult> HardDelete(int id)
+        {
+            bool res = await _Repository.HardDelete(id);
+            await _Repository.Save();
+
+            return res ? NoContent() : NotFound();
+        }
+    }
+}
