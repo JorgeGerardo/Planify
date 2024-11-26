f4fadc25ace45de0236a6a93fa7f5ecd3621b746
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-11-25 21:19:11 -0700

    Department controller added (it's works)

--- Planify/Controllers/DepartmentsController.cs
+++ Planify/Controllers/DepartmentsController.cs
@@ -1,97 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Planify.Models;
 using Planify.Repositories;
-using Planify.Services;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using static Planify.Services.Concurrency;
 
 namespace Planify.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    // Get's:
-    public partial class DepartmentsController : ControllerBase
+    public class DepartmentsController :
+        GenericController<Department, DepartmentRepository, DepartmentDTO>
     {
-        private readonly IGenericCRUDRepository<Department, int> _department;
+        public DepartmentsController(IGenericCRUDRepository<Department, int> _Repository) :
+            base(_Repository)
+        { }
 
-        public DepartmentsController(IGenericCRUDRepository<Department, int> department)
+        protected override Department MapToEntity(DepartmentDTO dto) =>
+            new Department { Name = dto.Name };
+
+        protected override Department MapToUpdateEntity(Department currentState, DepartmentDTO dto)
         {
-            _department = department;
+            currentState.Name = dto.Name;
+            return currentState;
         }
-
-
-        [HttpGet]
-        public async Task<IEnumerable<Department>> Get() =>
-            await _department.GetAll().ToListAsync();
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Department>> GetById(int id)
-        {
-            Department? res = await _department.GetById(id);
-            return res is not null ? res : NotFound();
-        }
-    }
-
-    //POST & PUT
-    public partial class DepartmentsController
-    {
-        [HttpPost]
-        public async Task<IActionResult> Add(DepartmentDTO newDeparment)
-        {
-            Department? NewDepartment = await _department
-                .Create(new Department { Name = newDeparment.Name });
-
-            await _department.Save();
-
-            if (NewDepartment is null)
-                return StatusCode(500);
-
-            return CreatedAtAction(nameof(Get), new { NewDepartment.Id }, NewDepartment);
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, DepartmentDTO deparmentChanges)
-        {
-            var dep = await _department.GetById(id);
-            if (dep is null)
-                return NotFound();
-
-            dep.Name = deparmentChanges.Name;
-            _department.Updated(dep);
-
-            ConcurrencyState state = await Concurrency.Check(() => _department.Save());
-
-            if (state == ConcurrencyState.ConcurrencyDetected)
-                return Conflict(new { message = ConflictMessage() });
-
-            return NoContent();
-        }
-
-    }
-
-    //Delete:
-    public partial class DepartmentsController
-    {
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            bool res = await _department.SoftDelete(id);
-            await _department.Save();
-
-            return res ? NoContent() : NotFound();
-        }
-
-
-        [HttpDelete("hardDelete/{id}")]
-        public async Task<IActionResult> HardDelete(int id)
-        {
-            bool res = await _department.HardDelete(id);
-            await _department.Save();
-
-            return res ? NoContent() : NotFound();
-        }
-
     }
 }
