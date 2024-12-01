dd2134ed56271db9955163997223ffb2d3e6a8fd
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-11-30 20:35:29 -0700

    Many to many relation added on ProjectTask

---
+++ Planify/Migrations/20241201033339_AddICollectionEmployee_ProjectTask.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Planify.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddICollectionEmployee_ProjectTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProjectTasks_Employees_EmployeeId",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectTasks_EmployeeId",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "ProjectTasks");
+
+            migrationBuilder.CreateTable(
+                name: "EmployeeProjectTask",
+                columns: table => new
+                {
+                    EmployeesId = table.Column<int>(type: "int", nullable: false),
+                    TasksId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeProjectTask", x => new { x.EmployeesId, x.TasksId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeProjectTask_Employees_EmployeesId",
+                        column: x => x.EmployeesId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeProjectTask_ProjectTasks_TasksId",
+                        column: x => x.TasksId,
+                        principalTable: "ProjectTasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeProjectTask_TasksId",
+                table: "EmployeeProjectTask",
+                column: "TasksId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeProjectTask");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "ProjectTasks",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTasks_EmployeeId",
+                table: "ProjectTasks",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProjectTasks_Employees_EmployeeId",
+                table: "ProjectTasks",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+    }
+}
--- Planify/Models/ProjectTask.cs
+++ Planify/Models/ProjectTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Planify.Models
 {
@@ -8,6 +9,30 @@ namespace Planify.Models
         public required Project Project { get; set; }
         public DateTime LastUpdateUTC { get; set; }
         //TODO: Falta un ICollection<Employee>
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
+        // También falta un estado:
+        //public bool IsCompleted { get; set; } = false;
+        //Una fecha que indique cuándo se completo:
+        //public TimeOnly? CompleteDate { get; set; } = null;
+        //public string Description { get; set; } = string.Empty;
+        //public DateTime CreatedDateUTC { get; set; } = DateTime.UtcNow;
+        //public DateTime? StartDate { get; set; }
+        //public DateTime? EstimatedEndDate { get; set; }
+
+        //public TaskStatus Status { get; set; } = TaskStatus.Pending;
+        //public required Employee AssignedBy { get; set; }
+        //public List<string> Comments { get; set; } = new List<string>();
+    }
+
+    //TODO: Agrega la conversión en FLUENT API
+    public enum TaskStatus
+    {
+        Pending,
+        InProgress,
+        Completed,
+        Blocked,
+        OnHold
     }
 
     public enum Priority
