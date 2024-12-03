c92b4da0987b355ed82a74d4a16f0557ce3d6144
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-12-03 13:59:26 -0700

    Removing comentaries

--- Planify/Repositories/ProjectRepository.cs
+++ Planify/Repositories/ProjectRepository.cs
@@ -3,14 +3,11 @@ using Planify.Models;
 
 namespace Planify.Repositories
 {
-    public class ProjectRepository : GenericRepository<Models.Project, int>
+    public class ProjectRepository : GenericRepository<Project, int>
     {
-        private string[] NavProperties = [nameof(Models.Project.Employees), nameof(Models.Project.Tasks)];
-        //private string[] NavProperties = ["Employees", "tasks"];
+        private string[] NavProperties = [nameof(Project.Employees), nameof(Project.Tasks)];
         public override string[] _NavigationProperties { get => NavProperties; }
 
-        public ProjectRepository(ProjectContext context) : base(context)
-        {
-        }
+        public ProjectRepository(ProjectContext context) : base(context) { }
     }
 }
