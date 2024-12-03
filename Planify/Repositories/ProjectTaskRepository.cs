19b0b5e80e5af3b47bd2691a4a4972a0cc227992
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-12-03 14:01:00 -0700

    Removing idead

---
+++ Planify/Repositories/ProjectTaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using Planify.Data;
+using Planify.Models;
+
+namespace Planify.Repositories
+{
+    public class ProjectTaskRepository : GenericRepository<ProjectTask, int>
+    {
+        //private string[] NavProperties = [nameof(ProjectTask.Employees), nameof(ProjectTask.Project)];
+        //public override string[] _NavigationProperties { get => NavProperties; }
+
+        public ProjectTaskRepository(ProjectContext context) : base(context) { }
+    }
+}
