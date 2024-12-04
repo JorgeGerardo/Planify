9d6b36ca0fdbaab909a3599b0758af2e3240ce7e
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-12-03 21:49:06 -0700

    Project task controller added

---
+++ Planify/Controllers/ProjectTaskController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Planify.Models;
+using Planify.Repositories;
+using Planify.Repositories.UoW;
+using System;
+using System.Threading.Tasks;
+
+namespace Planify.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public partial class ProjectTaskController : GenericController<ProjectTask, ProjectTaskRepository, ProjectTaskCreateDTO, ProjectTaskUpdateDTO>
+    {
+        private readonly IProjectManagement_UoW UoW;
+        public ProjectTaskController(IProjectManagement_UoW uow) :
+            base(uow.projectTasks)
+        { UoW = uow; }
+    }
+
+    public partial class ProjectTaskController
+    {
+        //Create:
+        protected override ProjectTask MapToEntity(ProjectTaskCreateDTO dto) =>
+            MapToEntityAsync(dto).GetAwaiter().GetResult();
+
+        protected override async Task<ProjectTask> MapToEntityAsync(ProjectTaskCreateDTO dto)
+        {
+            bool existeProject = await UoW.projects.ExistAsync(p => p.Id.Equals(dto.ProjectId));
+
+            if (!existeProject)
+                throw new Exception("El proyecto especificado no existe.");
+
+            return new ProjectTask
+            {
+                Name = dto.Name,
+                ProjectId = dto.ProjectId,
+                Description = dto.Description,
+                Priority = dto.Priority,
+                StartDate = dto.StartDate,
+                EstimatedEndDate = dto.EstimatedEndDate,
+            };
+        }
+
+
+        //Update
+        protected override ProjectTask MapToUpdateEntity(ProjectTask currentState, ProjectTaskUpdateDTO dto) =>
+            MapToUpdateEntityAsync(currentState, dto).GetAwaiter().GetResult();
+
+        protected override async Task<ProjectTask> MapToUpdateEntityAsync(ProjectTask currentState, ProjectTaskUpdateDTO dto)
+        {
+            bool exist = await UoW.projects.ExistAsync(p => p.Id.Equals(dto.ProjectId));
+            if (!exist)
+                throw new Exception("El proyecto especificado no existe.");
+
+            currentState.Name = dto.Name;
+            currentState.Description = dto.Description ?? currentState.Description;
+
+            currentState.StartDate = dto.StartDate ?? currentState.StartDate;
+            currentState.EstimatedEndDate = dto.EstimatedEndDate ?? currentState.EstimatedEndDate;
+            currentState.EstimatedEndDate = dto.EstimatedEndDate ?? currentState.EstimatedEndDate;
+
+            currentState.CompleteDate = dto.CompleteDate ?? currentState.CompleteDate;
+            currentState.Priority = dto.Priority ?? currentState.Priority;
+            currentState.Status = dto.Status;
+            currentState.IsCompleted = dto.IsCompleted;
+
+            return currentState;
+        }
+    }
+
+}
--- Planify/Models/ProjectTask.cs
+++ Planify/Models/ProjectTask.cs
@@ -7,27 +7,29 @@ namespace Planify.Models
     {
         //TODO: Creo que no se debería poder cambiar la tarea a asignarla a otro proyecto
         // tampoco la fecha de creación
+
+        //Nav properties
         public Project? Project { get; set; }
         public int ProjectId { get; set; }
-        public DateTime? LastUpdateUTC { get; set; }
         public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+
         public bool IsCompleted { get; set; } = false;
         public required string Description { get; set; }
+        public List<string> Comments { get; set; } = new List<string>();
+
 
         //Dates:
         public DateTime CreatedDateUTC { get; set; } = DateTime.UtcNow;
-        public DateTime? StartDate { get; set; }
-        public DateTime? EstimatedEndDate { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EstimatedEndDate { get; set; }
         public TimeOnly? CompleteDate { get; set; }
 
 
         public TaskStatus Status { get; set; } = TaskStatus.Pending;
         public required Priority Priority { get; set; }
 
-        public List<string> Comments { get; set; } = new List<string>();
     }
 
-    //TODO: Agrega la conversión en FLUENT API
     public enum TaskStatus
     {
         Pending,
@@ -44,23 +46,34 @@ namespace Planify.Models
 
     public class ProjectTaskCreateDTO
     {
+        public required string Name { get; set; }
         public int ProjectId { get; set; }
 
         public required Priority Priority { get; set; }
 
         //Dates:
-        public DateTime? StartDate { get; set; }
-        public DateTime? EstimatedEndDate { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EstimatedEndDate { get; set; }
 
         public required string Description { get; set; }
-
-        public ICollection<Employee>? Employees { get; set; } = new List<Employee>();
     }
 
 
     public class ProjectTaskUpdateDTO
     {
-        
+        public required string Name { get; set; }
+        public required int ProjectId { get; set; }
+        public required bool IsCompleted { get; set; }
+        public string? Description { get; set; }
+
+        public DateOnly? StartDate { get; set; }
+        public DateOnly? EstimatedEndDate { get; set; }
+
+        public TimeOnly? CompleteDate { get; set; }
+
+        public TaskStatus Status { get; set; } = TaskStatus.Pending;
+        public Priority? Priority { get; set; }
+
     }
 
 }
