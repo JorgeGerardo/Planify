98e0bfb00eb18669ab865ab017ab15b9eb7bd98f
Jorge Gerardo <jorgegerardo0105@gmail.com>
2024-12-06 06:10:03 -0700

    Remove Json cycles (all nav props use JsonIgnore)

--- Planify/Program.cs
+++ Planify/Program.cs
@@ -2,21 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Planify.Services;
-using Swashbuckle.AspNetCore.SwaggerUI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers();
 
-//builder.Services.AddControllers();
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
-    });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+
+//Swagger config
 BuilderConfigurationService.SetSwaggerGen(builder);
 
 //Scopes:
